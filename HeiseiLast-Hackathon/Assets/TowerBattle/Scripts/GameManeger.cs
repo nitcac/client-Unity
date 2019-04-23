c6fdc89f896c9c5e91973629cad914f760bcd17d
kamera <asahi20000124@icloud.com>
2019-04-24 03:38:32 +0900

    towerbattle用にmodelを追加、マウスクリックして話すとPutObjが落下するようにした

---
+++ HeiseiLast-Hackathon/Assets/TowerBattle/Scripts/GameManeger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+    Vector3 mousePos, zDistance = new Vector3(0, 0, 1);
+    Vector3 putPos;
+    [SerializeField]
+    GameObject putObj;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        mousePos = Input.mousePosition;
+        putPos = Camera.main.ScreenToWorldPoint((mousePos + zDistance));
+        putObj.transform.position = new Vector3(putPos.x, 0.6f, putPos.z);
+    }
+
+    private void OnMouseUp()//うまくよばれてなさそう
+    {
+        Debug.Log("put!");
+        putObj.GetComponent<PutObj>().Put();
+        putObj = null;
+    }
+}
---
+++ HeiseiLast-Hackathon/Assets/TowerBattle/Scripts/PutObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutObj : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Put()
+    {
+        rb.useGravity = true;
+    }
+}
