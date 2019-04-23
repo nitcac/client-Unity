970c94dfd1e8a77f2a0ec009834bd823475021f3
TomyJPN <masaya120331@gmail.com>
2019-04-23 16:36:39 +0900

    Vuforiaの構築，マーカー検知時の動作作成

---
+++ HeiseiLast-Hackathon/Assets/AR_Gengo/Scripts/AR_gameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AR_gameManager : MonoBehaviour {
+  string[] gengo = new string[]{"令和","平成","昭和","大正","明治","草","高専","安久","万字"};
+
+  [SerializeField]
+  Text GengoText;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+  public void markerFind() {
+    GengoText.text = gengo[Random.Range(0, gengo.Length)];
+  }
+
+}
