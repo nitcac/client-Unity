f46e31d5a4888757d19adbc5a211995d378b01ca
TomyJPN <masaya120331@gmail.com>
2019-04-25 17:01:43 +0900

    BGM,効果音演出とBPMによるミッション表示

--- HeiseiLast-Hackathon/Assets/AR_Gengo/Scripts/AR_gameManager.cs
+++ HeiseiLast-Hackathon/Assets/AR_Gengo/Scripts/AR_gameManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 using Vuforia;
 
 public class AR_gameManager : MonoBehaviour {
-  string[] gengo = new string[]{"令和","平成","昭和","大正","明治","草","高専","安久","万字"};
+  string[] gengo = new string[]{"令和","安明","安永","西米","元モ","草","高専","安久","万字"};
   bool isFinding=false;
+  bool isGameStart = false;
 
   CameraDevice cameraDevice;
 
@@ -25,6 +26,18 @@ public class AR_gameManager : MonoBehaviour {
   Text textBK;
   Text textRD;
 
+  [SerializeField]
+  Animator missionAnm;
+
+  //音楽関連
+  AudioSource audio_BGM;
+  AudioSource audio_cymbal;
+  int BPM = 126;
+  float totalTime;
+  float startTime;
+  int BPMNum=0;
+  float ratio_hasu=0;  //ratio再計算端数
+
   // Start is called before the first frame update
   void Start() {
     frashImg = canvas.transform.Find("flash").gameObject;
@@ -33,6 +46,32 @@ public class AR_gameManager : MonoBehaviour {
     textRD = canvas.transform.Find("textFrame/textRD").gameObject.GetComponent<Text>();
     textBK.text = "新元号ついに発表";
     textRD.text = "";
+
+    AudioSource[] audioSources = gameObject.GetComponents<AudioSource>();
+    audio_BGM = audioSources[0];
+    audio_cymbal = audioSources[1];
+    Invoke("GameStart", 3f);
+
+    startTime = Time.timeSinceLevelLoad+3f;
+    //一泊の秒数=60/テンポ
+    totalTime = 60f / (float)BPM;
+  }
+
+  void FixedUpdate() {
+    float diff = Time.timeSinceLevelLoad - startTime;
+    float ratio = diff / totalTime +ratio_hasu;
+    ratio_hasu = 0; //端数(ループの一度だけ適用すればいい)
+    if(ratio>=1f) {
+      ratio_hasu = ratio - 1f;
+      startTime = Time.timeSinceLevelLoad;
+      BPMNum++;
+      Debug.Log("BPM:"+(BPMNum%8+1));
+      //一時的に左側の条件を無効にしてます
+      if (BPMNum>16 && (BPMNum % 8 == 1 && Random.Range(0, 5) == 7) ||BPMNum%16==10 ) {
+        Debug.Log("mission");
+        missionAnm.SetTrigger("mission");
+      }
+    }
   }
 
   // Update is called once per frame
@@ -43,15 +82,18 @@ public class AR_gameManager : MonoBehaviour {
     else debugText.text = "デバッグ：左側";
 
   }
+
+  //マーカー認識時
   public void markerFind() {
     string newGengo = gengo[Random.Range(0, gengo.Length)];
+    audio_cymbal.Play();
     GengoText.text = newGengo;
     textBK.text = "新元号は「　　　」に決定";
     textRD.text=newGengo;
     isFinding = true;
     FlashON();
   }
-
+  //マーカーロスト時
   public void markerLost() {
     textBK.text = "新元号ついに発表";
     textRD.text = "";
@@ -59,7 +101,7 @@ public class AR_gameManager : MonoBehaviour {
   }
 
   void FlashOFF() {
-    //cameraDevice.SetFlashTorchMode(false);  //できん
+    //cameraDevice.SetFlashTorchMode(false);  //できん cameraDeviceの使い方が違う？
     frashImg.SetActive(false);
     if(isFinding)Invoke("FlashON", Random.Range(0.1f,0.5f));  //フラッシュ演出を続ける
   }
@@ -70,4 +112,9 @@ public class AR_gameManager : MonoBehaviour {
     Invoke("FlashOFF", 0.3f);
   }
 
+  void GameStart() {
+    Debug.Log("Play BGM");
+    isGameStart = true;
+    audio_BGM.Play();
+  }
 }
