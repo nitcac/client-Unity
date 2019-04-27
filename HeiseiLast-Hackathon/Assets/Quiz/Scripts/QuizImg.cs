c6f4854282157b3b4483f7c7245a8f0618d2c11e
TomyJPN <masaya120331@gmail.com>
2019-04-27 18:49:18 +0900

    Quiz画面，問題のドラッグアンドドロップの実装

---
+++ HeiseiLast-Hackathon/Assets/Quiz/Scripts/QuizImg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class QuizImg : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+  Rigidbody2D rg;
+
+  void Start() {
+    rg=GetComponent<Rigidbody2D>();
+    Debug.Log("でばっぐ");
+  }
+  void Update() {
+
+  }
+  
+  // ドラックが開始したとき呼ばれる.
+  public void OnBeginDrag(PointerEventData eventData) {
+    rg.gravityScale = 0;
+  }
+
+  // ドラック中に呼ばれる.
+  public void OnDrag(PointerEventData eventData) {
+    transform.position = eventData.position;
+  }
+
+  // ドラックが終了したとき呼ばれる.
+  public void OnEndDrag(PointerEventData eventData) {
+    rg.gravityScale = 10 ;
+  }
+
+}
